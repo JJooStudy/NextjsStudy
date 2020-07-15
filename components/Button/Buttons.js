516d4dd56013c73088fabc40281a3a8c6a233e28
jusstory <jjoo8829@naver.com>
2020-07-15 15:33:45 +0900

    Add button

--- components/Button/Buttons.js
+++ components/Button/Buttons.js
@@ -6,6 +6,7 @@ export const Button = styled.button`
     margin:0 5px;
     margin-bottom:7px;
     padding:7px 0;
+    box-sizing:border-box;
     border:1px solid #000;
     color:gray;
     font-size:14px;
@@ -28,14 +29,61 @@ export const Button = styled.button`
     `}
 
 `
-
+// 다음단계로 넘어가기
 export const ButtonStep = styled.button`
     display:block;
-    width:90%;
+    width:calc(100% - 20px);
     margin:0 auto;
     margin-top:30px;
+    margin-bottom:30px;
     padding:7px 0;
     border-radius:5px;
     color:#fff;
+    background-color:#000;
+`
+//아이 추가 
+export const ButtonAdd = styled.button`
+    display:block;
+    width:calc(100% - 30px);
+    margin:20px auto;
+    padding:10px 0;
+    :hover, :focus, :active{
+        background-color:#f1f1f1;
+    }
+    > span {
+        display:inline-block;
+        position:relative;
+        padding-left:30px;
+        ::before {
+            content:'';
+            display:block;
+            position:absolute;
+            top:4px;
+            left:4px;
+            width:3px;
+            height:11px;
+            background-color:#000;
+        }
+        ::after {
+            content:'';
+            display:block;
+            position:absolute;
+            top:8px;
+            left:0;
+            width:11px;
+            height:3px;
+            background-color:#000;
+        }
+    }
+    
+`
+//마지막 등록하기
+export const ButtonEnd = styled.button`
+    width:calc(100% + 20px);
+    margin-left:-10px;
+    padding:10px 0;
+    border-radius:0 0 5px 5px;
+    color:#fff;
+    font-size:15px;
     background-color:#000;
 `
--- containers/writing/Writing02.js
+++ containers/writing/Writing02.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Container from 'components/Container';
 import ButtonDiv from 'components/Button/ButtonDiv';
+import * as Button from 'components/Button/Buttons';
 import Inputs from 'components/Input/Inputs';
 import SaveDeleteDiv from 'components/Button/SaveDeleteDiv';
 
@@ -25,13 +26,16 @@ class Writing02 extends Component {
     render(){
         const { selected } = this.state
         return(
-            <Container>
-                <ButtonDiv handleSelect={this.handleSelect} selected={selected} data={gender} />
-                <Inputs label="아이에 대한 설명을 작성해 주세요" />
-                <Inputs label="분양 금액을 입력해 주세요" />
-                <SaveDeleteDiv />
-                <button></button>
-            </Container>
+            <>
+                <Container>
+                    <ButtonDiv handleSelect={this.handleSelect} selected={selected} data={gender} />
+                    <Inputs label="아이에 대한 설명을 작성해 주세요" />
+                    <Inputs label="분양 금액을 입력해 주세요" />
+                    <SaveDeleteDiv />
+                    <Button.ButtonAdd><span>아이 추가하기</span></Button.ButtonAdd>
+                    <Button.ButtonEnd>분양글 등록하기</Button.ButtonEnd>
+                </Container>
+            </>
         )
     }
 }
